03049c0e96a0599aab5a8fbf9083137915b20382
Suresh Kumar <kumarsk12072000@gmail.com>
2026-03-30 16:27:54 +0530

    utils are added

---
+++ backend/utils/admissionNumber.js
@@ -0,0 +1,20 @@
+const SeatMatrix = require('../models/seatmatrix.model');
+
+// Format: INST/2025/UG/CSE/KCET/0001
+// Counter is stored atomically in SeatMatrix.counters map
+const generateAdmissionNumber = async (program, institutionCode, quota, academicYear) => {
+  const year  = academicYear.split('-')[0];
+  const key   = `${quota}`;                   // map key in counters
+
+  // Atomically increment the counter for this quota
+  const matrix = await SeatMatrix.findOneAndUpdate(
+    { program: program._id, academicYear },
+    { $inc: { [`counters.${key}`]: 1 } },
+    { new: true }
+  );
+
+  const seq = String(matrix.counters.get(key)).padStart(4, '0');
+  return `${institutionCode}/${year}/${program.courseType}/${program.code}/${quota}/${seq}`;
+};
+
+module.exports = { generateAdmissionNumber };
